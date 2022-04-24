47a030ccf300efe1bface74746438c8c480737b8
TARAJESHA <rajeshata2019@gmail.com>
2022-04-24 22:22:54 +0530

    Checkpoint 1

---
+++ MovieBookingProject/src/App.js
@@ -0,0 +1,13 @@
+import Header from "./common/Header/Header";
+import Home from "./screens/home/home.js";
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Home></Home>
+    </>
+  );
+}
+
+export default App;
---
+++ MovieBookingProject/src/common/Header/Header.js
@@ -0,0 +1,29 @@
+import "./Header.css";
+import React from "react";
+import Button from "@material-ui/core/Button";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className="HeaderStyle">
+        <img
+          src="https://cdn.upgrad.com/uploads/production/286e1f11-1897-4d0c-ab0f-6b2bfc1ce642/logo.svg"
+          alt="logo"
+          className="logo"
+        />
+
+        {this.props.showBookShowButton === "true" ? (
+          <div className="bookshow-button">
+            <Button variant="contained" color="primary">
+              Book Show
+            </Button>
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
+    );
+  }
+}
+
+export default Header;
